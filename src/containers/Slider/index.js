4ac01d3dfbda26c84e6338522c72f4ff427594f4
Matt <160069337+MatthieuLa@users.noreply.github.com>
2024-07-14 10:33:22 +0200

    correction des erreurs dans la console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -23,36 +23,44 @@ const Slider = () => {
   const OptionChange = (e) => {
     setIndex(parseInt(e.target.value, 10));
   };
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.id}
-          className={`SlideCard SlideCard--${
-            index === idx ? "display" : "hide"
-          }`}
-        >
-          <img src={event.cover} alt={event.title} />
-          <div className="SlideCard__descriptionContainer">
-            <div className="SlideCard__description">
-              <h3>{event.title}</h3>
-              <p>{event.description}</p>
-              <div>{getMonth(new Date(event.date))}</div>
+      {byDateDesc?.map((event, idx) => {
+        const key = event.id || `event-${idx}`; // Use event.id if available, otherwise fallback to index
+        return (
+          <div
+            key={key}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt={event.title} />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
       <div className="SlideCard__paginationContainer">
         <div className="SlideCard__pagination">
-          {byDateDesc.map((event, radioIdx) => (
-            <input
-              key={`${event.id}`}
-              type="radio"
-              name="radio-button"
-              checked={index === radioIdx}
-              onChange={OptionChange}
-            />
-          ))}
+          {byDateDesc.map((event, radioIdx) => {
+            const key = event.id || `event-${radioIdx}`; // Use event.id if available, otherwise fallback to index
+            return (
+              <input
+                key={`radio-${key}`}
+                type="radio"
+                name="radio-button"
+                value={radioIdx}
+                checked={index === radioIdx}
+                onChange={OptionChange}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
